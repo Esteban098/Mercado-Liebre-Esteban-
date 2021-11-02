1bfc0043f821049c4a6e6cd793a62bc6fce071fc
Esteban <estebanlarcher098@gmail.com>
2021-11-02 16:24:27 -0300

    HTML 4 (añadio login y register)

--- app.js
+++ app.js
@@ -7,9 +7,17 @@ const publicPath = path.resolve(__dirname, "./public");
 app.use(express.static(publicPath));
 
 app.listen(3000, () => {
-  console.log("Server 3000 ON");
+    console.log("Server 3000 ON");
 });
 
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./views/home.html"));
+    res.sendFile(path.resolve(__dirname, "./views/home.html"));
+});
+
+app.get("/register", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/register.html"));
+});
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/login.html"));
 });
